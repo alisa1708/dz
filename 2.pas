9c05dadfee14e12bd436589d3b9c388b2f8e8ebb
Алиса Сидорчик <sidorchik@sfedu.ru>
2023-10-04 21:17:46 +0300

    В данной программе я реализовала функционал проверки введённого года на високосность

--- 2.pas
+++ 2.pas
@@ -1,3 +1,7 @@
 ﻿begin
-  Print('hallo')
+  var N:=ReadInteger('Введите год:');
+  if (N mod 4 = 0) or (N mod 100 = 0) and (N mod 400 <>0) then
+    Print ('366 дней.')
+  else
+    Print('365 дней.');
 end.
