fbd74243ae23cbb580a89124d20f466b814e4df8
Алиса Сидорчик <sidorchik@sfedu.ru>
2023-10-04 21:28:05 +0300

    Я написала программу ,которая читает с клавиатуры количество часов,минут и секунд,и возвращает суммарное количество секунд

---
+++ 3.pas
@@ -0,0 +1,9 @@
+﻿begin
+  var A:=ReadInteger('Введите количество часов:');
+  var B:= ReadInteger('Введите количество минут:');
+  var C:=ReadInteger('Введите количество часов:');
+  A*=3600;
+  B*=60;
+  C+=A+B;
+  Print(C,'секунд');
+end.
